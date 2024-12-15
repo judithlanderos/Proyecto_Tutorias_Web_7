5e01a149a7996314b6aeff82d59bfaf336b55197
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-12-15 00:07:47 -0600

    29.Commit Proyecto Android

--- app/src/main/java/com/example/proyecto_tutorias_web_7/ActivityLanding.java
+++ app/src/main/java/com/example/proyecto_tutorias_web_7/ActivityLanding.java
@@ -1,4 +1,35 @@
 package com.example.proyecto_tutorias_web_7;
 
-public class ActivityLanding {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+        
+        Button btnRegistro = findViewById(R.id.btn_registro);
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegistro = new Intent(ActivityLanding.this, ActivityRegistro.class);
+                startActivity(intentRegistro);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLogin = new Intent(ActivityLanding.this, ActivityLogin.class);
+                startActivity(intentLogin);
+            }
+        });
+    }
 }
